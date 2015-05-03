b2b05ca879f23c459d08c78bedffd496962131d8
Jonathan Berk <jdsberk@gmail.com>
2015-05-02 17:26:05 -0700

    Set up Rails-API and added basic gems

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+Superduber::Application.config.secret_token = 'b802bd98abc2a2dd989388715df603ad4d69f602e513540c6a8386868aa3829817bf9f221fd3ad9632cd38b8a1be2284061f8704f7bb8bb495b437d48bfd8bc0'
