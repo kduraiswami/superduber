7c190f22e549e7ab5b1e9c9d08644e572db33705
Tony Mai <thetonymai@gmail.com>
2015-05-10 19:03:04 -0400

    Refactor twilio cancel_ride webhook"

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -56,22 +56,12 @@ class UsersController < ApplicationController
   end
 
   def cancel_ride # Webhook triggered by user SMS to Twilio
-
-    #Find the event based on phone number
-    #event.cancel_ride to trigger delete request
-    #Send success message to user if receive status 204 from Uber
-
     user_response = params[:Body]
     p user = User.find_by(phone: params[:From])
     p event = user.next_event
     event.cancel_ride
-    # if user_response == "666"
-      #request ride
-    # else
-      #return sms saying price has changed
-    # end
 
-    render json: {}
+    render json: { message: "success" }
   end
 
 
