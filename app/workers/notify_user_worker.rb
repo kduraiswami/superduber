03262c4e9d8b30951601f487c9df979cc57178df
Jonathan Berk <jdsberk@gmail.com>
2015-05-14 00:38:22 -0700

    Fixed background workers

--- app/workers/notify_user_worker.rb
+++ app/workers/notify_user_worker.rb
@@ -1,14 +1,12 @@
 class NotifyUserWorker
   @queue = :notify_user
 
-  def self.perform(event)
-    #Check uber time
-    event.twilio_upcoming_event_notification
+  def self.perform(event_hash)
+    puts "INSIDE NotifyUserWorker background job.  EVENT:"
+    puts event_hash
+    event_id = event_hash['_id']['$oid']
+    event = Event.find(event_id) # this returns the actual event object, not just a hash of its data
+    event.twilio_upcoming_event_notification #Send twilio notification
 
-    #Send twilio notification
-
-
-
-    puts "Inside NotifyUserWorker background job"
   end
 end
--- app/workers/request_estimate_worker.rb
+++ app/workers/request_estimate_worker.rb
@@ -1,7 +1,9 @@
 class RequestEstimateWorker
   @queue = :request_estimate
 
-  def self.perform(event)
+  def self.perform(event_hash)
+    event_id = event_hash['_id']['$oid']
+    event = Event.find(event_id) # returns actual event object
     event.schedule_bg_job
     puts "Inside RequestEstimateWorker background job"
   end
