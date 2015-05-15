76568969e1c29da3ca60bceb97efc883ddb7713e
Jonathan Berk <jdsberk@gmail.com>
2015-05-15 01:29:32 -0700

    Create event working and setting up background jobs correctly

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -15,8 +15,8 @@ class EventsController < ApplicationController
   def create
     user = User.find_by(uuid: params[:user_id])
     new_event = user.events.create(event_params)
-
     p new_event.update_ride_id!
+    new_event.schedule_bg_job
     redirect_to "/"
   end
 
