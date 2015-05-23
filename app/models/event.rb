8ffe1b3dd7582ab0e05b51084096ccae5ab4bd22
Jonathan Berk <jdsberk@gmail.com>
2015-05-22 21:43:01 -0700

    Fixed timezone issue after it broke

--- app/models/event.rb
+++ app/models/event.rb
@@ -20,12 +20,13 @@ class Event
   geocoded_by :geocode_user_addresses
   before_validation :geocode,
     :if => lambda{ |obj| obj.depart_address_changed? || obj.arrival_address_changed? }
-  before_validation :update_estimate!, :update_ride_id!
+  before_validation :update_ride_id, :update_estimate
+  before_validation :adjust_for_local_time, #must happen after geocoding; is there a better way to ensure the order?
+      :if => lambda{ |obj| obj.arrival_datetime_changed? }
 
   validates_presence_of :name, :arrival_datetime
   validate :arrival_address_found, :depart_address_found, :depart_arrival_address_not_same, :date_is_not_in_the_past, :ride_id_not_found, :pickup_estimate_not_found
 
-  before_save :adjust_for_local_time
 
   def geocode_user_addresses
     depart_search_results = Geocoder.search(depart_address)[0]
@@ -70,13 +71,13 @@ class Event
     end
   end
 
-  def ride_id_not_found
+  def ride_id_not_found #depends on successful "update_ride_id!""
     unless ride_id
       errors.add(:ride_name, "can't be found for that departure address; please confirm you entered it correctly, and that this type of ride is available in this city.")
     end
   end
 
-  def pickup_estimate_not_found
+  def pickup_estimate_not_found #depends on successful "update_estimate!"
     unless pickup_estimate
       errors.add(:pickup_estimate, "unavailable. Check that the distance do not exceed 100 miles.")
     end
@@ -84,7 +85,8 @@ class Event
 
   ######## TIME ZONE ADJUSTMENTS ########
   def adjust_for_local_time
-    input_time = self.arrival_datetime #What the user entered; Mongo defaults to save as UTC
+    puts "Input time from user:"
+    p input_time = self.arrival_datetime #What the user entered; Mongo defaults to save as UTC
     timezone = Timezone::Zone.new(latlon: self.depart_coords)
     puts "Local timezone of event: #{timezone.zone}"
 
@@ -108,7 +110,7 @@ class Event
     (self.duration_estimate + self.pickup_estimate).minutes
   end
 
-  def update_estimate!
+  def update_estimate
     puts "RIDE ESTIMATE RESPONSE:"
     p response = request_estimate_response(self)
 
@@ -117,12 +119,16 @@ class Event
     else
       self.pickup_estimate = response['pickup_estimate']
       self.duration_estimate = (response['trip']['duration_estimate']/60.0).ceil
-      self.save!
 
       puts "ESTIMATED DURATION: #{estimated_duration} (minutes)"
-
       response
     end
+  end
+
+  def update_estimate!
+    response = update_estimate
+    self.save!
+    response
   end
 
   def notification_buffer
@@ -130,7 +136,7 @@ class Event
   end
 
   def time_to_notify_user
-    self.arrival_datetime - estimated_duration - notification_buffer
+    self.arrival_datetime_local - estimated_duration - notification_buffer
   end
 
   def schedule_bg_job
@@ -171,7 +177,7 @@ class Event
     end
   end
 
-  def update_ride_id!
+  def update_ride_id
     p "Updating ride ID"
     p "SELF.USER.UBER_ACCESS_TOKEN: #{self.user.uber_access_token}"
     p response = HTTParty.get("https://api.uber.com/v1/products",
@@ -185,11 +191,10 @@ class Event
       }
     )
 
-    puts "Response from update_ride_id! method: #{response}"
+    puts "Response from update_ride_id method: #{response}"
     response["products"].each do |product|
       if self.ride_name.downcase == product["display_name"].downcase
         self.ride_id = product["product_id"]
-        self.save!
         return self.ride_id
       end
     end
